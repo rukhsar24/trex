b58562c27fe89991bc4f7964c7b723845af5d4ee
rukhsar24 <74099983+rukhsar24@users.noreply.github.com>
2020-12-08 13:02:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  
+}
+
+function draw(){
+  
+  background("white")
+  
+  drawSprites();
+
+}
